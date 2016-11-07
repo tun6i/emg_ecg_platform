540466d700e6652a1352c81558997aafcaec0bce
[benjamin] <[schirlebenjamin@outlook.de]>
2016-11-07 16:55:10 +0100

    Material Design Template

---
+++ Fachstudie_Template/app/src/main/java/activity/PlotFragment.java
@@ -0,0 +1,43 @@
+package activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import de.fachstudie.fachstudie_template.R;
+
+public class PlotFragment extends Fragment {
+
+    public PlotFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_plot, container, false);
+
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
