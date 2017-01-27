f53fde665c5bc9743b1bf2a5acaeacf55d7697ed
Tung Anh Nguyen <tunganh.nguyen@t-online.de>
2017-01-27 11:21:36 +0100

    Remove unused Bitmap and Canvas

--- Fachstudie_Template/app/src/main/java/activity/HeatmapFragment.java
+++ Fachstudie_Template/app/src/main/java/activity/HeatmapFragment.java
@@ -23,34 +23,6 @@ import java.util.Random;
 import de.fachstudie.fachstudie_template.R;
 
 public class HeatmapFragment extends Fragment {
-
-    private class CustomDrawableView extends View {
-        private ShapeDrawable mDrawable;
-
-        public CustomDrawableView(Context context) {
-            super(context);
-
-            int x = 100;
-            int y = 100;
-            int width = 300;
-            int height = 50;
-
-            mDrawable = new ShapeDrawable(new OvalShape());
-            mDrawable.getPaint().setColor(0xff74AC23);
-            mDrawable.setBounds(x, y, x + width, y + height);
-        }
-
-        protected void onDraw(Canvas canvas) {
-            mDrawable.draw(canvas);
-        }
-    }
-    Bitmap b = Bitmap.createBitmap(600, 800, Bitmap.Config.ARGB_8888);
-    Canvas c = new Canvas(b);
-    Paint paint = new Paint();
-    ImageView view;
-
-
-
     public HeatmapFragment() {
 
     }
@@ -63,26 +35,6 @@ public class HeatmapFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragment_heatmap, container, false);
-
-
-        /*view = (ImageView) rootView.findViewById(R.id.imageView2);
-        view.setClickable(true);
-        view.setImageBitmap(b);
-
-        paint.setAntiAlias(true);
-        paint.setStrokeWidth(6F);
-        paint.setColor(Color.BLUE);
-        paint.setStyle(Paint.Style.STROKE);
-        c.drawLine(0, 0, 500, 500, paint);
-        view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                c.drawLine(123,123,0,0, paint);
-            }
-        });*/
-
-
-
 
 
         return rootView;
