00f7d410a7b44f5dab984ab91dc2df63d3618d8c
Tung Anh Nguyen <tunganh.nguyen@t-online.de>
2017-01-29 13:04:10 +0100

    Minor fixes

--- Fachstudie_Template/app/src/main/java/activity/HeatmapFragment.java
+++ Fachstudie_Template/app/src/main/java/activity/HeatmapFragment.java
@@ -1,6 +1,7 @@
 package activity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -17,13 +18,21 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import java.io.IOException;
 import java.util.Random;
 
+import bluetooth.BluetoothSetup;
 import de.fachstudie.fachstudie_template.R;
 
 public class HeatmapFragment extends Fragment {
+
+    private BluetoothSetup btSetup = BluetoothSetup.getInstance();
+    private ImageButton imgButton;
+    private final int CONNECTION_ESTABLISHED = 1;
+
 
     public HeatmapFragment() {
     }
@@ -36,8 +45,7 @@ public class HeatmapFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragment_heatmap, container, false);
-
-
+        imgButton = (ImageButton) rootView.findViewById(R.id.imgbutton2);
 
         /*ShapeDrawable sd = new ShapeDrawable(new OvalShape());
         sd.setIntrinsicHeight(100);
@@ -45,6 +53,26 @@ public class HeatmapFragment extends Fragment {
         sd.getPaint().setColor(Color.parseColor("#abcd123"));
         ImageView iv = (ImageView) rootView.findViewById(R.id.imageView);
         iv.setBackground(sd);*/
+
+        imgButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!btSetup.isConnected()) {
+                    Intent intent = new Intent(getActivity(), ShowPairedDevices.class);
+                    startActivityForResult(intent, CONNECTION_ESTABLISHED);
+                } else {
+                    try {
+                        btSetup.getBtData().close();
+                        btSetup.getBtSocket().close();
+                        btSetup.setConnected(false);
+                        imgButton.setImageResource(R.drawable.ic_bluetooth_no_connection);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        });
 
         return rootView;
     }
